87065630a1580f7922be063cf2d8e66bfc44166d
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-30 20:24:33 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -39,7 +39,14 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name:"
+  response = gets.chomp
+  if my_songs.keys.include?(response)
+    puts "Playing #{response}"
+    open <#{my_songs[response]}>
+  else
+    puts "Invalid input, please try again."
+  end
 end
 
 def exit_jukebox
